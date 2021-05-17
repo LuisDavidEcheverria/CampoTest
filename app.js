d5755a6858701f4ffce13f39997e50a549a1c1d3
David Echeverria <luisdavidecheverriaperez@gmail.com>
2021-05-17 01:45:40 -0500

    Puerto corregido

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + '/public')) 
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',(req,res)=>{
-    res.send('XD')
-})
+app.get("/", (req, res) => {
+  res.send("XD");
+});
 
-app.listen(port,()=>{
-    console.log('Servidor escuchando en el puerto',port)
-})
+app.listen(process.env.PORT || 5000);
